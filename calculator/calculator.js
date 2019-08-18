f480da39e53d1d1452dddcd171af970c51fc461d
ojaoc <c0rreia@hotmail.com>
2019-08-18 03:27:42 +0100

    exercise complete

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,40 @@
-function add () {
-	
+function add(a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum(num) {
+	// const values = Array.from(num);
+	if (num.length == 0) {
+		return 0;
+	}
+	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+	return num.reduce(reducer);
 }
 
-function multiply () {
-	
+function multiply (num) {
+	// const values = Array.from(num);
+	const reducerMultiply = (accumulator, currentValue) => accumulator * currentValue;
+	return num.reduce(reducerMultiply);
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if(num === 0) {
+		return 1;
+	}
+	const values = [];
+	const reducerFactorial = (accumulator, currentValue) => accumulator * currentValue;
+	for(let i=num; i>=1; i--) {
+		values.push(i)
+	}
+	return values.reduce(reducerFactorial);
 }
 
 module.exports = {
