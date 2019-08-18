3d15dc6909c9c7fb73cc849ef63980c9c4915f6d
ojaoc <c0rreia@hotmail.com>
2019-08-18 04:56:00 +0100

    exercise complete

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = (str) => {
+    const lowerCase = str.toLowerCase();
+    const lettersOnly = lowerCase.replace(/()[^A-Z]/gi, '');
+    const reverseString = lettersOnly.split("").reverse().join("");
+    if (lettersOnly == reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
 module.exports = palindromes
