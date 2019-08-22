9296ab0932c50c91bcd1f7fe8c8efbfc54887003
ojaoc <c0rreia@hotmail.com>
2019-08-22 22:51:21 +0100

    exercise completed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+    if (int < 0) {
+        return 'OOPS';
+    }
+    let numberSequence = [1,1];
+    let i = 0;
+    do {
+        numberSequence.push(numberSequence[numberSequence.length-1] + numberSequence[numberSequence.length-2]);
+        i++;
+    } 
+    while (i < int); 
+    return numberSequence[int-1];
 }
-
 module.exports = fibonacci
